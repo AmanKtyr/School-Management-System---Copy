b7e4bf2501dbbd12e4ad387be8f7c465db50aaf7
Aman Ktyr <amanktyrrr@gmail.com>
2025-11-15 00:15:56 +0530

    Fix: Update deploy.sh to use python3 for manage.py commands

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 pip install -r requirements.txt
-python SMS/manage.py migrate --noinput
-python SMS/manage.py collectstatic --noinput
+python3 SMS/manage.py migrate --noinput
+python3 SMS/manage.py collectstatic --noinput
